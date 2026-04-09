c3039151bb8fc358e9e4daa399728300836fd17f
kyohyun kim <86403089+kyohyunkim@users.noreply.github.com>
2026-04-09 09:49:52 +0900

    Update update.js

--- update.js
+++ update.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const URLS = {
     // gid가 포함된 최종 CSV 주소로 교체하세요.
-    sheet1: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRjkerfUM4Af5iYw4IBlHSdQHRP61U18YAeNoKzL3_1-0GdiQdLtcPMeGgu_nhGqDMbnIaZBpvpfow6/pub?output=csv',
+    sheet1: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSuGnGQb0WGZk5myNrkVF2d-OLRsAPEd8HY9PM-cVsuC5xiBDCN3tSrRaZWgUJqA2hQmFFHA3OiOWyb/pub?output=csv',
     sheet2: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ8hV7tT_22c1L85H_Lc0LRmL5NrQcpyuC_xlVK1u53NTjAXNqgPoVngczVuOY60DfOEMa_g9EpDl0J/pub?output=csv'
 };
 
